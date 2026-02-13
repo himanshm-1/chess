d218202adca2a391db161b30f7568c8c3866422a
himanshm-1 <himanshm@proton.me>
2026-02-13 09:58:46 +0530

    Abstraction

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,32 @@
-use macroquad::prelude::*;
-use macroquad::ui::root_ui;
-use miniquad::conf::Icon;
+mod utils;
+use macroquad::{prelude::*};
+use std::collections::HashMap;
 
-#[derive(PartialEq, Eq)]
-enum Mode {
-    Menu,
-    White,
-    Black,
-}
-
-#[macroquad::main(window_conf)]
+#[macroquad::main(conf)]
 async fn main() {
-    let _square_position = set_square_position(&mut [(0, 0); 64], 0, 0);
+    let mut _bitboards: HashMap<&str, u64> = HashMap::new();
+    _bitboards = utils::set_bitboard(_bitboards); // Naming Scheme: BRook, WKnight, BTotal, WTotal, Total etc.
 
-    let mut _bitboards: [u64; 15] = [
-        0b10000001_00000000_00000000_00000000_00000000_00000000_00000000_00000000, // BRook:      0
-        0b01000010_00000000_00000000_00000000_00000000_00000000_00000000_00000000, // BKnight:    1
-        0b00100100_00000000_00000000_00000000_00000000_00000000_00000000_00000000, // BBishop:    2
-        0b00001000_00000000_00000000_00000000_00000000_00000000_00000000_00000000, // BKing:      3
-        0b00010000_00000000_00000000_00000000_00000000_00000000_00000000_00000000, // BQueen:     4
-        0b00000000_11111111_00000000_00000000_00000000_00000000_00000000_00000000, // BPawn:      5
-        0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_10000001, // WRook:      6
-        0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_01000010, // WKnight:    7
-        0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00100100, // WBishop:    8
-        0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00010000, // WQueen:     9
-        0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00001000, // WKing:     10
-        0b00000000_00000000_00000000_00000000_00000000_00000000_11111111_00000000, // WPawn:     11
-        0b11111111_11111111_00000000_00000000_00000000_00000000_00000000_00000000, // BOccupied: 12
-        0b00000000_00000000_00000000_00000000_00000000_00000000_11111111_11111111, // WOccupied: 13
-        0b11111111_11111111_00000000_00000000_00000000_00000000_11111111_11111111, // TOccupied: 14
-    ];
+    let board_white: Texture2D = utils::white_board().await;
+    let board_black: Texture2D = utils::black_board().await;
+    let _piece_array: Texture2D = load_texture("chess_pieces_array.png").await.unwrap();
 
-    let board_white: Texture2D = load_texture("chess_white_perspective_board.png")
-        .await
-        .unwrap();
+    next_frame().await; // To initialize the window so, window_height and width are not zero.
+    let board_pos = utils::board_pos(&board_white);
+    let _square_position = utils::set_square_position(&mut [(0., 0.); 64], board_pos.0, board_pos.1);
+    let mut _piece_index: HashMap<&str, i32> = HashMap::new();
+    _piece_index = utils::set_piece_index(_piece_index);
 
-    let board_black: Texture2D = load_texture("chess_black_perspective_board.png")
-        .await
-        .unwrap();
-
-    let mut mode = Mode::Menu;
-
-    next_frame().await;
-
-    let board_pos: (f32, f32) = (
-        (window_conf().window_width / 2) as f32 - board_white.width() / 2.,
-        (window_conf().window_height / 2) as f32 - board_white.height() / 2.,
-    );
+    let mut mode = utils::Mode::Menu;
 
     loop {
         clear_background(GOLD);
 
-        let height = (window_conf().window_height / 2 - 40) as f32;
-
-
-
-        if mode == Mode::Menu {
-            root_ui().label(
-                Some(Vec2::new(
-                    (window_conf().window_width / 2) as f32
-                        - measure_text("Choose which color you want to play as.", None, 15, 1.0)
-                            .width/2.,
-                    height,
-                )),
-                "Choose which color you want to play as.",
-            );
-            if root_ui().button(
-                Some(Vec2::new(
-                    (window_conf().window_width / 2 - 60) as f32,
-                    height + 20.,
-                )),
-                "Black",
-            ) {
-                mode = Mode::Black;
-            } else if root_ui().button(
-                Some(Vec2::new(
-                    (window_conf().window_width / 2 + 20) as f32,
-                    height + 20.,
-                )),
-                "White",
-            ) {
-                mode = Mode::White;
-            }
-        } else if mode == Mode::Black {
+        if mode == utils::Mode::Menu {
+            mode = utils::ui(mode);
+        } else if mode == utils::Mode::Black {
             draw_texture(&board_black, board_pos.0, board_pos.1, WHITE);
-        } else if mode == Mode::White {
+        } else if mode == utils::Mode::White {
             draw_texture(&board_white, board_pos.0, board_pos.1, WHITE);
         }
 
@@ -92,36 +34,10 @@ async fn main() {
     }
 }
 
-fn set_square_position(
-    square_position: &mut [(isize, isize); 64],
-    offset_x: isize,
-    offset_y: isize,
-) -> [(isize, isize); 64] {
-    let mut current = 0;
-    for j in 0..8 {
-        for i in 0..8 {
-            square_position[current] = (offset_x + i * 32, offset_y + j * 32);
-            current += 1;
-        }
-    }
-    return *square_position;
+
+/* I don't know why this happens but I can't put a function from another module directly into macroquad::main() */
+fn conf() -> Conf {
+    utils::window_conf()
 }
 
-fn set_icon() -> Icon {
-    const LOGO16: &[u8; 1024] = include_bytes!("logo16.rgba");
-    const LOGO32: &[u8; 4096] = include_bytes!("logo32.rgba");
-    const LOGO64: &[u8; 16384] = include_bytes!("logo64.rgba");
-    Icon {
-        small: *LOGO16,
-        medium: *LOGO32,
-        big: *LOGO64,
-    }
-}
-fn window_conf() -> Conf {
-    Conf {
-        window_title: "Chess".to_owned(),
-        window_resizable: false,
-        icon: Some(set_icon()),
-        ..Default::default()
-    }
-}
+
---
+++ src/utils.rs
@@ -0,0 +1,180 @@
+use macroquad::{prelude::*, ui::root_ui};
+use miniquad::conf::Icon;
+use std::collections::HashMap;
+
+pub fn window_conf() -> Conf {
+    Conf {
+        window_title: "Chess".to_owned(),
+        window_resizable: false,
+        icon: Some(set_icon()),
+        ..Default::default()
+    }
+}
+
+
+pub fn ui(mut mode: Mode) ->Mode{
+    root_ui().label(label_pos(), "Choose which color you want to play as.");
+    if root_ui().button(black_pos(), "Black") {
+        mode = Mode::Black;
+    } else if root_ui().button(white_pos(), "White") {
+        mode = Mode::White;
+    }
+    mode
+}
+
+pub fn q_width() -> f32 {
+    measure_text("Choose which color you want to play as.", None, 15, 1.0).width / 2.
+}
+
+pub fn height() -> f32 {
+    (window_conf().window_height / 2 - 40) as f32
+}
+
+pub fn set_piece_index(mut _piece_index: HashMap<&str, i32>) -> HashMap<&str, i32> {
+    _piece_index.insert("BRook", 3);
+    _piece_index.insert("BKnight", 4);
+    _piece_index.insert("BBishop", 5);
+    _piece_index.insert("BKing", 2);
+    _piece_index.insert("BQueen", 1);
+    _piece_index.insert("BPawn", 6);
+    _piece_index.insert("WRook", 9);
+    _piece_index.insert("WKnight", 10);
+    _piece_index.insert("WBishop", 11);
+    _piece_index.insert("WQueen", 7);
+    _piece_index.insert("WKing", 8);
+    _piece_index.insert("WPawn", 12);
+    _piece_index
+}
+
+pub fn board_pos(board_white: &Texture2D) -> (f32, f32) {
+    (
+        (window_conf().window_width / 2) as f32 - board_white.width() / 2.,
+     (window_conf().window_height / 2) as f32 - board_white.height() / 2.,
+    )
+}
+
+pub async fn white_board() -> Texture2D {
+    load_texture("chess_white_perspective_board.png")
+    .await
+    .unwrap()
+}
+
+pub async fn black_board() -> Texture2D {
+    load_texture("chess_black_perspective_board.png")
+    .await
+    .unwrap()
+}
+
+pub fn black_pos() -> Option<Vec2> {
+    Some(Vec2::new((width() - 60) as f32, height() + 20.))
+}
+pub fn white_pos() -> Option<Vec2> {
+    Some(Vec2::new((width() + 20) as f32, height() + 20.))
+}
+
+#[derive(PartialEq, Eq)]
+pub enum Mode {
+    Menu,
+    White,
+    Black,
+}
+
+pub fn set_square_position(
+    square_position: &mut [(f32, f32); 64],
+                       offset_x: f32,
+                       offset_y: f32,
+) -> [(f32, f32); 64] {
+    let mut current = 0;
+    for j in 0..8 {
+        for i in 0..8 {
+            square_position[current] = (offset_x + i as f32 * 32., offset_y + j as f32 * 32.);
+            current += 1;
+        }
+    }
+    *square_position
+}
+
+pub fn set_icon() -> Icon {
+    const LOGO16: &[u8; 1024] = include_bytes!("logo16.rgba");
+    const LOGO32: &[u8; 4096] = include_bytes!("logo32.rgba");
+    const LOGO64: &[u8; 16384] = include_bytes!("logo64.rgba");
+    Icon {
+        small: *LOGO16,
+        medium: *LOGO32,
+        big: *LOGO64,
+    }
+}
+
+
+pub fn set_bitboard(mut _bitboards: HashMap<&str, u64>) -> HashMap<&str, u64> {
+    _bitboards.insert(
+        "BRook",
+        0b10000001_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
+    );
+    _bitboards.insert(
+        "BKnight",
+        0b01000010_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
+    );
+    _bitboards.insert(
+        "BBishop",
+        0b00100100_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
+    );
+    _bitboards.insert(
+        "BKing",
+        0b00001000_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
+    );
+    _bitboards.insert(
+        "BQueen",
+        0b00010000_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
+    );
+    _bitboards.insert(
+        "BPawn",
+        0b00000000_11111111_00000000_00000000_00000000_00000000_00000000_00000000,
+    );
+    _bitboards.insert(
+        "WRook",
+        0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_10000001,
+    );
+    _bitboards.insert(
+        "WKnight",
+        0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_01000010,
+    );
+    _bitboards.insert(
+        "WBishop",
+        0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00100100,
+    );
+    _bitboards.insert(
+        "WQueen",
+        0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00010000,
+    );
+    _bitboards.insert(
+        "WKing",
+        0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00001000,
+    );
+    _bitboards.insert(
+        "WPawn",
+        0b00000000_00000000_00000000_00000000_00000000_00000000_11111111_00000000,
+    );
+    _bitboards.insert(
+        "BTotal",
+        0b11111111_11111111_00000000_00000000_00000000_00000000_00000000_00000000,
+    );
+    _bitboards.insert(
+        "WTotal",
+        0b00000000_00000000_00000000_00000000_00000000_00000000_11111111_11111111,
+    );
+    _bitboards.insert(
+        "Total",
+        0b11111111_11111111_00000000_00000000_00000000_00000000_11111111_11111111,
+    );
+
+    _bitboards
+}
+
+pub fn width() -> i32 {
+    window_conf().window_width / 2
+}
+
+pub fn label_pos() -> Option<Vec2> {
+    Some(Vec2::new(width() as f32 - q_width(), height()))
+}
